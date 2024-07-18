a45fbec7ce7a569f2d0af7c865133197b62e9a77
Karina <umiralikarina@gmail.com>
2024-07-18 18:37:14 +0500

    cleaned the code

--- client/src/components/CreateForm.js
+++ client/src/components/CreateForm.js
@@ -1,21 +1,85 @@
-import * as yup from "yup";
-import {useForm, Controller} from "react-hook-form";
-import {yupResolver} from "@hookform/resolvers/yup";
+import { useState } from "react";
+import axios from "axios";
+import { Button, IconButton} from '@mui/material';
 
-export const CreateForm = ({onSubmit, setVisible, handlePhoto}) => {
-    const schema = yup.object().shape({
-        name: yup.string().required('Введите имя'),
-        secondName: yup.string().required('Введите фамилию'),
-        address: yup.string().required('Введите адрес'),
-        email: yup.string().required('Введите почту'),
-        phoneNumber: yup.string().required('Введите номер телефона'),
-    })
+export const CreateForm = ({setVisible}) => {
+    const [newUser, setNewUser] = useState({
+        name: '',
+        secondName: '',
+        address: '',
+        email: '',
+        phoneNumber: '',
+        photo: '',
+    });
 
-    const { handleSubmit, control } = useForm({
-        resolver: yupResolver(schema),
-    })
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('name', newUser.name);
+        formData.append('secondName', newUser.secondName);
+        formData.append('address', newUser.address);
+        formData.append('email', newUser.email);
+        formData.append('phoneNumber', newUser.phoneNumber);
+        formData.append('photo', newUser.photo);
+
+        axios.post("http://localhost:3001/users", formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+            .then(() => {
+                alert("Новый пользователь добавлен в список");
+                setVisible(false);
+                window.location.reload();
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    const handleChange = (e) => {
+        setNewUser({...newUser, [e.target.name]: e.target.value});
+    };
+
+    const handlePhoto = (e) => {
+        setNewUser({...newUser, photo: e.target.files[0]});
+    };
+
+    const inputArr = [
+        {"placeholder": "Имя", "name": "name", "value": newUser.name},
+        {"placeholder": "Фамилия", "name": "secondName", "value": newUser.secondName},
+        {"placeholder": "Адрес", "name": "address", "value": newUser.address},
+        {"placeholder": "Почта", "name": "email", "value": newUser.email},
+        {"placeholder": "Номер телефона", "name": "phoneNumber", "value": newUser.phoneNumber},
+    ];
 
     return (
-        <h1>moved</h1>
+        <div className='form-container'>
+            <div className='form-header'>
+                <h3>Добавление нового пользователя</h3>
+                <IconButton onClick={() => setVisible(false)} type="button">X</IconButton>
+            </div>
+            <form onSubmit={handleSubmit} encType='multipart/form-data' className='form'>
+                {inputArr.map((input) => (
+                <input
+                    className='form-item'
+                    type='text'
+                    key={input.name}
+                    placeholder={input.placeholder}
+                    name={input.name}
+                    value={input.value}
+                    onChange={handleChange}
+                />
+                ))}
+                <input
+                    className="file"
+                    type="file"
+                    accept='.png, .jpg, .jpeg'
+                    name="photo"
+                    onChange={handlePhoto}
+                />
+                <Button className='btn' variant="contained" type='submit'>Создать</Button>
+            </form>
+        </div>
     )
 }
--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,17 +1,10 @@
 import * as React from 'react';
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Button, IconButton, Avatar, ListItemAvatar, ListItemText, Divider} from '@mui/material';
+import { Button, Avatar, ListItemAvatar, ListItemText, Divider} from '@mui/material';
+import { CreateForm } from './CreateForm';
 
 export const ListOfUsers = () => {
-    const [newUser, setNewUser] = useState({
-        name: '',
-        secondName: '',
-        address: '',
-        email: '',
-        phoneNumber: '',
-        photo: '',
-    });
     const [users, setUsers] = useState([]);
     const [visible, setVisible] = useState(false);
 
@@ -27,39 +20,6 @@ export const ListOfUsers = () => {
         fetchUsers();
     }, []);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append('name', newUser.name);
-        formData.append('secondName', newUser.secondName);
-        formData.append('address', newUser.address);
-        formData.append('email', newUser.email);
-        formData.append('phoneNumber', newUser.phoneNumber);
-        formData.append('photo', newUser.photo);
-
-        axios.post("http://localhost:3001/users", formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        })
-            .then(() => {
-                alert("Новый пользователь добавлен в список");
-                setVisible(false);
-                window.location.reload();
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    }
-
-    const handleChange = (e) => {
-        setNewUser({...newUser, [e.target.name]: e.target.value});
-    };
-
-    const handlePhoto = (e) => {
-        setNewUser({...newUser, photo: e.target.files[0]});
-    }
-
     const handleAdd = () => {
         setVisible(true);
     };
@@ -74,46 +34,15 @@ export const ListOfUsers = () => {
         }
     };
 
-    const inputArr = [
-        {"placeholder": "Имя", "name": "name", "value": newUser.name},
-        {"placeholder": "Фамилия", "name": "secondName", "value": newUser.secondName},
-        {"placeholder": "Адрес", "name": "address", "value": newUser.address},
-        {"placeholder": "Почта", "name": "email", "value": newUser.email},
-        {"placeholder": "Номер телефона", "name": "phoneNumber", "value": newUser.phoneNumber},
-    ]
-
     return (
         <div className='container'>
             <div className='component'>
                 {visible && 
-                <div className='form-container'>
-                    <div className='form-header'>
-                        <h3>Добавление нового пользователя</h3>
-                        <IconButton onClick={() => setVisible(false)} type="button">X</IconButton>
-                    </div>
-                    <form onSubmit={handleSubmit} encType='multipart/form-data' className='form'>
-                        {inputArr.map((input) => (
-                            <input
-                                className='form-item'
-                                type='text'
-                                placeholder={input.placeholder}
-                                name={input.name}
-                                value={input.value}
-                                onChange={handleChange}
-                            />
-                        ))}
-                        <input
-                            type="file"
-                            accept='.png, .jpg, .jpeg'
-                            name="photo"
-                            onChange={handlePhoto}
-                        />
-                        <Button className='btn' variant="contained" type='submit'>Создать</Button>
-                    </form>
-                </div>
+                    <CreateForm setVisible={setVisible} />
                 }
-                <Button type='button' onClick={handleAdd} variant="outlined">+ Добавить пользователя</Button>
+                
                     <div className='list'>
+                    <Button type='button' onClick={handleAdd} variant="outlined">+ Добавить пользователя</Button>
                         {users.map((user) => (
                             <div key={user._id}>
                                 <ListItemAvatar>
